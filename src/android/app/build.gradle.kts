ee8fc3fa15be018bdb6733d8b03689301ee05adc
izzy2lost <izzynochill@gmail.com>
2025-10-22 02:44:22 -0400

    material icons added

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -222,6 +222,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     testImplementation(libs.junit)
     testImplementation(libs.archunit.junit4)
     androidTestImplementation(libs.androidx.junit)
