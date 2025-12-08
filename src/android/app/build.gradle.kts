5313787a5c74389f34cb25e8c84ef5652e4acf06
Izzy <izzynochill@gmail.com>
2025-12-08 04:29:41 -0500

    Update build.gradle.kts

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -102,8 +102,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_21)
-        targetCompatibility(JavaVersion.VERSION_21)
+        sourceCompatibility(JavaVersion.VERSION_17)
+        targetCompatibility(JavaVersion.VERSION_17)
     }
 
     externalNativeBuild {
@@ -151,7 +151,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "21"
+        jvmTarget = "17"
     }
 }
 
